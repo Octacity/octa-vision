538adc5bed4db9c59f17b3d642f359d090ca741c
Ganadev Prajapathy <ganadev@octacity.org>
2025-04-26 00:20:53 +0000

    Can you remove the sign up from landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,6 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { useRouter } from "next/navigation";
-
 const LandingPage = () => {
-  const router = useRouter();
-
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
@@ -29,28 +17,6 @@ const LandingPage = () => {
         <p className="mt-3 text-xl text-gray-600">
           Let your cameras talk to you
         </p>
-
-        <div className="mt-6 flex flex-wrap items-center justify-around max-w-4xl sm:w-full">
-          <Card className="w-96">
-            <CardHeader>
-              <CardTitle className="text-2xl">Sign Up</CardTitle>
-              <CardDescription>
-                Create an account to get started.
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <form
-                onSubmit={() => router.push('/signup')}
-                className="flex flex-col space-y-4"
-              >
-                <div>
-                  Ready to get Started?
-                </div>
-                <Button type="submit">Sign Up</Button>
-              </form>
-            </CardContent>
-          </Card>
-        </div>
       </main>
 
       <footer className="flex items-center justify-center w-full border-t">
